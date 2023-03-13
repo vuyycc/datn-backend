15d13ef9ae42f685b9a99f2f0be487ea2697ea06
Tran Doan Vu <doanvu17061999@gmail.com>
2023-03-13 11:14:13 +0700

    update

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const dotenv = require('dotenv')
-//const http = require('http').Server(app)
-const https = require(`https`).Server(app);
+const http = require('http').Server(app)
+//const https = require(`https`).Server(app);
 
+// var corsOptions = {
+//   origin: ["http://localhost:3000","http://127.0.0.1:8000","https://patient-covid-19.onrender.com"]
+// };
 var corsOptions = {
-  origin: ["http://localhost:3000","http://127.0.0.1:8000","https://patient-covid-19.onrender.com"]
-};
-
+    origin: '*'
+  };
+  
 
 dotenv.config()
 app.use(express.json())
@@ -17,6 +20,7 @@ app.use(cors(corsOptions))
 
 let allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     next();
@@ -44,5 +48,5 @@ db.sequelize.sync()
 
 const PORT=process.env.PORT
 
-https.listen(PORT, () => { console.log("Server started on http://localhost:" + PORT) })
+http.listen(PORT, () => { console.log("Server started on http://localhost:" + PORT) })
 module.exports = app;
